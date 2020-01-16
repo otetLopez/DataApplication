407d9f115dee7547a7c28759f0deef2dbc252eeb
otetLopez <otet.tud@gmail.com>
2020-01-16 17:10:24 -0500

    Initial Commit

---
+++ DataApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataApplication
+//
+//  Created by otet_tud on 1/16/20.
+//  Copyright © 2020 otet_tud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
